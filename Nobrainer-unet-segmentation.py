448873f01a935825be182d39b46ccaa1e99e7261
ncullen93 <nicholas.cullen@med.lu.se>
2024-04-23 16:45:30 +0200

    update examples

--- Nobrainer-unet-segmentation.py
+++ Nobrainer-unet-segmentation.py
@@ -1,11 +1,12 @@
 ## Example: Train aparc segmentation model using nitrain with UNET architecture from nobrainer
 
+import os
 import nitrain
 from nitrain.readers import ColumnReader, ComposeReader
 from nitrain import transforms as tx
 
 # note: have to convert mgz to nii.gz manually because mgz is not supported by ntimage yet
-csv_path = '/Users/ni5875cu/Desktop/nobrainer/filepaths.csv'
+csv_path = os.path.join('~/Desktop/nobrainer/filepaths.csv')
 
 dataset = nitrain.Dataset(
     inputs=ColumnReader(csv_path, 'features', is_image=True),
