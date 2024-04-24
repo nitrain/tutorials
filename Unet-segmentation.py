dcf156535f8d07cdd2d7c73e12c5dbc54dc90220
ncullen93 <nicholas.cullen@med.lu.se>
2024-04-24 14:53:43 +0200

    update nobrainer example

--- Unet-segmentation.py
+++ Unet-segmentation.py
@@ -1,39 +1,51 @@
 ## Example: Train aparc segmentation model using nitrain with UNET architecture from nobrainer
 
-import nitrain
-from nitrain.readers import ColumnReader, ComposeReader
+import nitrain as nt
+from nitrain.samplers import SliceSampler
+from nitrain.readers import ColumnReader
 from nitrain import transforms as tx
 
 # note: have to convert mgz to nii.gz manually because mgz is not supported by ntimage yet
 csv_path = '/Users/ni5875cu/Desktop/nobrainer/filepaths.csv'
 
-dataset = nitrain.Dataset(
+dataset = nt.Dataset(
     inputs=ColumnReader(csv_path, 'features', is_image=True),
     outputs=ColumnReader(csv_path, 'labels', is_image=True),
     transforms={
         ('inputs','outputs'): [tx.Resample((128,128,128))],
         'outputs': [tx.Cast('uint8'),
-                    tx.CustomFunction(lambda img: (img == 4) | (img == 44))]
+                    tx.CustomFunction(lambda img: (img == 4) | (img == 43))]
     }
 )
 
 # get an example input + output from the dataset
 x, y = dataset[0]
 
-loader = nitrain.Loader(dataset,
-                        images_per_batch=4)
+# plot example with segmentation overlay
+x.plot(y)
+
+loader = nt.Loader(dataset,
+                   images_per_batch=4,
+                   sampler=SliceSampler(batch_size=24))
 
 # get an example batch from the loader
 xbatch, ybatch = next(iter(loader))
 
-arch_fn = nitrain.fetch_architecture('unet', dim=3)
-model = arch_fn((128,128,128,1), number_of_outputs=1,
-                 number_of_layers=2, number_of_filters_at_base_layer=12)
 
-trainer = nitrain.Trainer(model,
-                          task='segmentation')
+arch_fn = nt.fetch_architecture('unet', dim=2)
+model = arch_fn((128,128,1), 
+                number_of_outputs=1,
+                number_of_layers=2, 
+                number_of_filters_at_base_layer=12,
+                mode='sigmoid')
+
+trainer = nt.Trainer(model,
+                     task='segmentation')
 
 # see information about how the trainer is compiled
 print(trainer)
 
 trainer.fit(loader, epochs=5)
+
+
+pred = model.predict(loader.to_keras())
